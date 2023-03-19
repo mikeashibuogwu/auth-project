c50ba3a445b01d43aecc460980a40d6a5175eb37
Michael Ashibuogwu <damimyke@gmail.com>
2023-03-19 16:08:27 +0100

    add a project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp }  from 'vue'
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
 
-createApp(App).mount('#app')
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "https://gabbyblog.herokuapp.com/";
+
+axios.interceptors.response.use(undefined, function(error) {
+  if (error) {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      store.dispatch("LogOut");
+      return router.push("/login");
+    }
+  }
+});
+
+
+
+
+createApp(App)
+.use(router)
+store
+.mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,60 @@
+import { createRouter, createWebHistory } from "vue-router";
+import store from "../store";
+import Home from "../views/HomeView.vue";
+import Register from "../views/RegisterView.vue";
+import Login from "../views/LoginView.vue";
+import Posts from "../views/ProductsView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+    meta: { guest: true },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: { guest: true },
+  },
+  {
+    path: "/posts",
+    name: "Posts",
+    component: Posts,
+    meta: { requiresAuth: true },
+  },
+];
+
+const router = createRouter({ history: createWebHistory(), routes });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (store.getters.isAuthenticated) {
+      next();
+      return;
+    }
+    next("/login");
+  } else {
+    next();
+  }
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.guest)) {
+    if (store.getters.isAuthenticated) {
+      next("/posts");
+      return;
+    }
+    next();
+  } else {
+    next();
+  }
+});
+
+export default router;
